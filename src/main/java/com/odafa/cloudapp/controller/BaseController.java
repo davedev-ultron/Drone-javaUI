d5988f886c98175397ba4e18c3011b24b7cd4186
Gerardo Calvillo <gerry@Gerardos-MacBook-Pro.local>
2021-04-22 00:04:11 -0500

    video controller

--- src/main/java/com/odafa/cloudapp/controller/BaseController.java
+++ src/main/java/com/odafa/cloudapp/controller/BaseController.java
@@ -4,15 +4,24 @@ import java.io.BufferedReader;
 import java.io.InputStreamReader;
 import java.net.URL;
 
+import com.odafa.cloudapp.configuration.ConfigReader;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 
 @Slf4j
 @Controller
+@RequiredArgsConstructor 
 public class BaseController {
+
+    // read variables from yaml
+	private final ConfigReader configurations;
+
 
     @GetMapping("/")
     public String indexPage() {
@@ -20,4 +29,32 @@ public class BaseController {
         log.debug("Index Page Opened");
         return "index";
     }
+    
+    @GetMapping("/v/{droneId}")
+	public String getVideoFeed(Model model, @PathVariable("droneId") String droneId) {
+		
+        // model is used to send the object to front end
+        // variables are named the same as in video.hmtl
+		model.addAttribute("publicIp", getPublicIpAddress());
+		model.addAttribute("droneId", droneId);
+		model.addAttribute("videoEndpoint", configurations.getVideoWsEndpoint());
+        
+        return "video";
+    }
+
+    //dynamically get ip address
+	private String getPublicIpAddress() {
+		String ip = "";
+		try {
+			final URL whatismyip = new URL("http://checkip.amazonaws.com");
+
+			try(final BufferedReader in = new BufferedReader(new InputStreamReader(whatismyip.openStream()))){
+				ip = in.readLine();
+			}
+
+		} catch (Exception e) {
+			log.error(e.getMessage());
+		}
+		return ip;
+	}
 }
