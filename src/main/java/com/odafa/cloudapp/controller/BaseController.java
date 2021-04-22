259b865cd0ae895a7b6cb664117e1650f675643a
Gerardo Calvillo <gerry@Gerardos-MacBook-Pro.local>
2021-04-21 21:56:25 -0500

    added logging

--- src/main/java/com/odafa/cloudapp/controller/BaseController.java
+++ src/main/java/com/odafa/cloudapp/controller/BaseController.java
@@ -8,11 +8,16 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
 @Controller
 public class BaseController {
 
     @GetMapping("/")
     public String indexPage() {
+        
+        log.debug("Index Page Opened");
         return "index";
     }
 }
